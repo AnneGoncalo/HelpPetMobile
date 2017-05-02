7b3b044d763f05b087ead623314ba39374c88106
AnneGoncalo <annegoncalo@gmail.com>
2017-05-02 19:54:56 -0300

    commit inicial

---
+++ app/src/main/java/com/example/anne/helppet/DenunciasActivity.java
@@ -0,0 +1,13 @@
+package com.example.anne.helppet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DenunciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denuncias);
+    }
+}
